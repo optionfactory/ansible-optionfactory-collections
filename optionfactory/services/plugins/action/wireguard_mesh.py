3c619d9c0b18259bdb28187a9d99a6cf3ce9d11f
Roberto Ferranti <roberto.ferranti@gmail.com>
2026-05-13 10:19:20 +0200

    :ref: cleanup

--- optionfactory/services/plugins/action/wireguard_mesh.py
+++ optionfactory/services/plugins/action/wireguard_mesh.py
@@ -2,24 +2,48 @@ from ansible_collections.optionfactory.services.plugins.module_utils.actions imp
 
 
 class ActionModule(Action):
+    ARGUMENT_SPEC = {
+        'host_ip': {'type': 'str', 'required': True},
+        'interface': {'type': 'str', 'default': 'wg-mesh'},
+        'config_template': {'type': 'str', 'default': 'wireguard_mesh_config.j2'},
+        'peers': {
+            'type': 'list',
+            'required': True,
+            'elements': 'dict',
+            'options': {
+                'host_ip': {'type': 'str', 'required': True},
+                'tunnel_cidr': {'type': 'str', 'required': True},
+                'docker_subnet': {'type': 'str', 'required': True},
+                'private_key': {'type': 'str', 'required': True, 'no_log': True},
+                'public_key': {'type': 'str', 'required': True},
+            }
+        }
+    }
 
-    ARGUMENT_SPEC = dict(
-        host_ip=dict(type='str', required=True),
-        interface=dict(type='str', required=False, default='wg-mesh'),
-        config_template=dict(type='str', required=False, default='wireguard_mesh_config.j2'),
-        peers=dict(
-            type='list',
-            required=True,
-            elements='dict',
-            options=dict(
-                host_ip=dict(type='str', required=True),
-                tunnel_cidr=dict(type='str', required=True),
-                docker_subnet=dict(type='str', required=True),
-                private_key=dict(type='str', required=True, no_log=True),
-                public_key=dict(type='str', required=True),
-            )
-        )
-    )
+    def run(self, tmp=None, task_vars=None):
+        args, ctx = super(ActionModule, self).run(tmp, task_vars)
+        wg_interface = args.get('interface')
+        config_template = args.get('config_template')
+        err, local, remote_peers = self.partition_peers(args.get('host_ip'), args.get('peers') or [])
+        if err:
+            return err
+        err, wireguard_package_changed = self.install_packages(ctx)
+        if err:
+            return err
+        err, ip_forward_changed = self.enable_ip_forward(ctx)
+        if err:
+            return err
+        err, wireguard_config_changed = self.configure_wireguard(ctx, wg_interface, local, remote_peers, config_template)
+        if err:
+            return err
+        err, docker_overrides_changed = self.configure_docker(ctx, wg_interface)
+        if err:
+            return err
+        return {
+            'msg': "Wireguard mesh setup completed.",
+            'failed': False,
+            'changed': (wireguard_package_changed or ip_forward_changed or wireguard_config_changed or docker_overrides_changed)
+        }
 
     def partition_peers(self, host_ip, peers):
         local = None
@@ -35,27 +59,18 @@ class ActionModule(Action):
             return failure("there are no configured remote peers"), None, []
         return None, local, remote_peers
 
-    def run(self, tmp=None, task_vars=None):
-        args, ctx = super(ActionModule, self).run(tmp, task_vars)
-
-        wg_interface = args.get('interface')
-
-        err, local, remote_peers = self.partition_peers(args.get('host_ip'), args.get('peers') or []) 
-        if err:
-            return err
-
-        err, wireguard_package_changed = self.action_step(ctx, {
-            'step': f"Ensuring wireguard is installed",
+    def install_packages(self, ctx):
+        return self.action_step(ctx, {
+            'step': f"Ensuring wireguard and iptables are installed",
             'name': 'ansible.builtin.package',
             'args': {
-                'name': 'wireguard',
+                'name': ['wireguard', 'iptables'],
                 'state': 'present'
             }
         })
-        if err:
-            return err
 
-        err, ip_forward_changed = self.module_step(ctx, {
+    def enable_ip_forward(self, ctx):
+        return self.module_step(ctx, {
             'step': "Ensuring net.ipv4.ip_forward is enabled",
             'name': 'ansible.posix.sysctl',
             'args': {
@@ -63,13 +78,12 @@ class ActionModule(Action):
                 'value': '1',
                 'sysctl_set': True,
                 'state': 'present',
-                'reload': True                
+                'reload': True
             }
         })
-        if err: 
-            return err
 
-        err, wireguard_dir_changed = self.module_step(ctx, {
+    def configure_wireguard(self, ctx, wg_interface, local, remote_peers, config_template):
+        err, directory_changed = self.module_step(ctx, {
             'step': "Ensuring /etc/wireguard directory is present",
             'name': 'ansible.builtin.file',
             'args': {
@@ -78,32 +92,44 @@ class ActionModule(Action):
                 'mode': '0700'
             }
         })
-        if err: 
-            return err
-
-        err, config_template = self.find_template(args.get('config_template'))
         if err:
-            return err
+            return err, False
+        err, config_template = self.find_template(config_template)
+        if err:
+            return err, False
         svc_ctx = ctx.with_updated_vars({
             'wg_interface': wg_interface,
             'local': local,
             'remote_peers': remote_peers,
         })
-        err, wireguard_config_changed = self.action_step(svc_ctx, {
-            'step': f"Ensuring { wg_interface } configuration is up to date",
+        err, config_changed = self.action_step(svc_ctx, {
+            'step': f"Ensuring {wg_interface} configuration is up to date",
             'name': 'ansible.builtin.template',
             'args': {
                 'src': config_template,
-                'dest': f'/etc/wireguard/{ wg_interface }.conf',
+                'dest': f'/etc/wireguard/{wg_interface}.conf',
                 'owner': 'root',
                 'group': 'root',
                 'mode': '0600'
             }
         })
         if err:
-            return err
-        
-        err, docker_override_dir_changed = self.module_step(ctx, {
+            return err, False
+        err, service_changed = self.module_step(ctx, {
+            'step': 'Ensuring service is started and updated',
+            'name': 'ansible.builtin.systemd',
+            'args': {
+                'name': f'wg-quick@{wg_interface}',
+                'enabled': True,
+                'state': 'restarted' if config_changed else 'started'
+            }
+        })
+        if err:
+            return err, False
+        return err, directory_changed or config_changed or service_changed
+
+    def configure_docker(self, ctx, wg_interface):
+        err, directory_changed = self.module_step(ctx, {
             'step': "Ensuring Docker systemd override directory exists",
             'name': 'ansible.builtin.file',
             'args': {
@@ -114,28 +140,26 @@ class ActionModule(Action):
                 'mode': '0755'
             }
         })
-        if err: 
-            return err
-        
-        err, docker_override_changed = self.module_step(ctx, {
+        if err:
+            return err, False
+        err, overrides_changed = self.module_step(ctx, {
             'step': "Ensuring Docker waits for WireGuard interface",
             'name': 'ansible.builtin.copy',
             'args': {
                 'content': "\n".join([
                     "[Unit]",
-                    f"After=wg-quick@{ wg_interface }.service",
-                    f"Wants=wg-quick@{ wg_interface }.service"
+                    f"After=wg-quick@{wg_interface}.service",
+                    f"Wants=wg-quick@{wg_interface}.service"
                 ]),
-                'dest': f'/etc/systemd/system/docker.service.d/wireguard-{ wg_interface }.conf',
+                'dest': f'/etc/systemd/system/docker.service.d/wireguard-{wg_interface}.conf',
                 'owner': 'root',
                 'group': 'root',
                 'mode': '0644',
             }
         })
-        if err: 
-            return err
-
-        if docker_override_changed:
+        if err:
+            return err, False
+        if overrides_changed:
             err, _ = self.module_step(ctx, {
                 'step': "Reloading systemd daemon to apply Docker dependency",
                 'name': 'ansible.builtin.systemd',
@@ -143,30 +167,6 @@ class ActionModule(Action):
                     'daemon_reload': True
                 }
             })
-            if err: 
-                return err
-            
-        err, service_changed = self.module_step(ctx, {
-            'step': 'Ensuring service is started and updated',
-            'name': 'ansible.builtin.systemd',
-            'args': {
-                'name': f'wg-quick@{ wg_interface }',
-                'enabled': True,
-                'state': 'restarted' if wireguard_config_changed else 'started'
-            }
-        })
-        if err:
-            return err        
-        return {
-            'msg': "Wireguard mesh setup completed.",
-            'failed': False,
-            'changed': (
-                wireguard_package_changed or
-                ip_forward_changed or
-                wireguard_dir_changed or
-                wireguard_config_changed or
-                docker_override_dir_changed or
-                docker_override_changed or
-                service_changed
-            )
-        }
+            if err:
+                return err, False
+        return None, directory_changed or overrides_changed
